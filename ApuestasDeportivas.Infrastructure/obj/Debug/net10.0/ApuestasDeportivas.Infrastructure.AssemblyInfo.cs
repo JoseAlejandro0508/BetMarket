f627d7c78c227127a519f815e7b6ed52be631892
alejandro20050830 <96395408+alejandro20050830@users.noreply.github.com>
2026-04-19 01:23:55 -0400

    v2.0

--- ApuestasDeportivas.Infrastructure/obj/Debug/net10.0/ApuestasDeportivas.Infrastructure.AssemblyInfo.cs
+++ ApuestasDeportivas.Infrastructure/obj/Debug/net10.0/ApuestasDeportivas.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApuestasDeportivas.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88385141a46cdc7e0a49215b21c190ad20b6cbd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbaaea3477b80a3a9314000d91758896a569b449")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApuestasDeportivas.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApuestasDeportivas.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
